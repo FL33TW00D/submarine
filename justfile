7d97cf046f116f57d85c73208633cfaee8eac7bb
FL33TW00D <chris@fleetwood.dev>
2026-01-01 18:54:00 +0000

    chore: eager >> compile

---
+++ justfile
@@ -0,0 +1,5 @@
+tc_ncu:
+    ncu --profile-from-start off -o profile uv run tc.py
+
+eager_ncu:
+    ncu --profile-from-start off -o profile uv run eager.py
--- tc.py
+++ tc.py
@@ -3,12 +3,18 @@ import torch.nn.functional as F
 import triton
 
 DEVICE = triton.runtime.driver.active.get_active_torch_device()
-
 torch.manual_seed(0)
 x = torch.randn(1823, 781, device=DEVICE)
-y_torch = F.layer_norm(x, (x.shape[-1],))
 
-# compiled_fn = torch.compile(
-#    lambda: F.layer_norm(x, (x.shape[-1],)), mode="max-autotune-no-cudagraphs"
-# )
-# compiled_fn()
+compiled_fn = torch.compile(
+    lambda: F.layer_norm(x, (x.shape[-1],)), mode="max-autotune-no-cudagraphs"
+)
+
+for _ in range(3):
+    compiled_fn()
+torch.cuda.synchronize()
+
+torch.cuda.cudart().cudaProfilerStart()
+compiled_fn()
+torch.cuda.synchronize()
+torch.cuda.cudart().cudaProfilerStop()
