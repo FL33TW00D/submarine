281d72924a836e9427ea402c4c52261e7ff4f166
FL33TW00D <chris@fleetwood.dev>
2026-01-05 14:27:26 +0000

    chore: liger slow on bf16

--- main.py
+++ main.py
@@ -18,16 +18,12 @@ def layernorm_fwd_kernel(
     BLOCK_SIZE: tl.constexpr,
 ):
     pid = tl.program_id(0)
-
     offs = tl.arange(0, BLOCK_SIZE)
     irange = pid * N + offs
     x = tl.load(x_ptr + irange, offs < N, other=0.0)
-
     mu = tl.sum(x) / N
     x_shift = x - mu
-
     var = tl.sum(x_shift * x_shift) / N
-
     x_norm = x_shift * tl.rsqrt(var + eps)
     tl.store(output_ptr + irange, x_norm, offs < N)
 
@@ -35,7 +31,6 @@ def layernorm_fwd_kernel(
 def our_ln(x: torch.Tensor, eps: float = 1e-5):
     output = torch.empty_like(x)
     assert x.device == DEVICE and output.device == DEVICE
-
     M, N = output.shape
     BLOCK_SIZE = triton.next_power_of_2(N)
     layernorm_fwd_kernel[(M, 1, 1)](x, output, M, N, eps, BLOCK_SIZE=BLOCK_SIZE)
@@ -47,7 +42,6 @@ x = torch.randn(1823, 781, device=DEVICE)
 y_triton = our_ln(x)
 y_torch = F.layer_norm(x, (x.shape[-1],))
 assert torch.allclose(y_triton, y_torch, atol=1e-2, rtol=1e-2), (y_triton, y_torch)
-
 # print PTX out
 if False:
     device = torch.cuda.current_device()
@@ -73,7 +67,7 @@ class Kernel(enum.Enum):
         line_arg="provider",  # argument name whose value corresponds to a different line in the plot
         line_vals=[
             "torch",
-            "triton",
+            "custom",
             "torch_compile",
             "liger",
         ],
@@ -87,35 +81,34 @@ class Kernel(enum.Enum):
 def benchmark(M, N, provider):
     x = torch.rand((M, N), device=DEVICE, dtype=torch.bfloat16)
     quantiles = [0.5, 0.2, 0.8]
-
     norm_shape = (x.shape[-1],)
 
-    if provider == "torch":
-        ms, min_ms, max_ms = triton.testing.do_bench(
-            lambda: F.layer_norm(x, norm_shape),
-            quantiles=quantiles,
-        )
-    elif provider == "triton":
-        ms, min_ms, max_ms = triton.testing.do_bench(
-            lambda: our_ln(x),
-            quantiles=quantiles,
-        )
-    elif provider == "torch_compile":
-        # must use max-autotune-no-cudagraphs https://x.com/gaunernst/status/2005524873477812298?s=20
-        compiled_fn = torch.compile(
-            lambda: F.layer_norm(x, norm_shape), mode="max-autotune-no-cudagraphs"
-        )
-        compiled_fn()  # warm
-        ms, min_ms, max_ms = triton.testing.do_bench(
-            compiled_fn,
-            quantiles=quantiles,
-        )
-    elif provider == "liger":
-        ln = LigerLayerNorm(hidden_size=norm_shape).cuda()
-        ms, min_ms, max_ms = triton.testing.do_bench(
-            lambda: ln(x),
-            quantiles=quantiles,
-        )
+    match Kernel(provider):
+        case Kernel.TCH:
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: F.layer_norm(x, norm_shape),
+                quantiles=quantiles,
+            )
+        case Kernel.CUSTOM:
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: our_ln(x),
+                quantiles=quantiles,
+            )
+        case Kernel.TCH_CMP:
+            compiled_fn = torch.compile(
+                lambda: F.layer_norm(x, norm_shape), mode="max-autotune-no-cudagraphs"
+            )
+            compiled_fn()  # warm
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                compiled_fn,
+                quantiles=quantiles,
+            )
+        case Kernel.LIGER:
+            ln = LigerLayerNorm(hidden_size=norm_shape).cuda()
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: ln(x),
+                quantiles=quantiles,
+            )
 
     def gbps(ms):
         return 2 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
