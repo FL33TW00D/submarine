0c6acf9c59cf867ab3777f83374be4342a69310e
FL33TW00D <chris@fleetwood.dev>
2026-01-17 14:33:30 +0000

    chore: mixture of concenrs

--- cute_ramp/softmax.py
+++ cute_ramp/softmax.py
@@ -42,6 +42,9 @@ def _softmax_fwd(x: cute.Tensor, y: cute.Tensor):
     mi = thread_idx // n
 
     tr = t_reduce(x[(None, (mi, ni))], cute.ReductionOp.MAX)  # reduce thread and warp
+    tv_layout = cute.make_layout(shape=((2, 2), (2, 3)), stride=((2, 12), (1, 4)))
+    composed = cute.composition(tensor, tv_layout)
+    my_data = composed[(tidx, None)]  # Just use local tidx!
 
 
 """
@@ -59,9 +62,14 @@ def softmax(x: cute.Tensor, y: cute.Tensor):
     threads_per_block = min(256, N // vpt)
     num_warps = threads_per_block // cute.arch.WARP_SIZE
 
-    # smem = cutlass.utils.SmemAllocator()
-    # smem_layout = cute.make_layout((1, num_warps))
-    # sR = smem.allocate_tensor(cutlass.Float16, smem_layout)
+    smem = cutlass.utils.SmemAllocator()
+    # Each thread does an LDG128 and loads 8 values.
+    # It then does an 8 value reduction
+    # Then we do a warp reduction, so 32 threads get their values reduced to 1
+    # They then write this into shared memory
+
+    smem_layout = cute.make_layout((1, num_warps))
+    sR = smem.allocate_tensor(cutlass.Float16, smem_layout)
 
     print(f"Threads per block {threads_per_block}")
     print(f"Num warps: {num_warps}")
