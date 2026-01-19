3e9d399882889fba73cfe5f13acaba9fed3b144c
FL33TW00D <chris@fleetwood.dev>
2026-01-19 15:37:11 +0000

    chore: triton got hands

--- cute_ramp/softmax.py
+++ cute_ramp/softmax.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import cutlass.cute as cute
 import torch
 from cutlass.cute.runtime import from_dlpack
@@ -14,26 +15,24 @@ Plan:
 
 
 @cute.jit
-def block_reduce_single_row(val: cute.Numeric, reduction_buffer: cute.Tensor) -> cute.Numeric:
+def block_row_reduce(
+    val: cute.Numeric, op: Callable, reduction_buffer: cute.Tensor, init_val: cute.Numeric = 0.0
+) -> cute.Numeric:
     lane_idx, warp_idx = cute.arch.lane_idx(), cute.arch.warp_idx()
     warps_per_row = reduction_buffer.shape[0]
     col_idx = warp_idx % warps_per_row
     if lane_idx == 0:
         reduction_buffer[col_idx] = val  # Only lane 0 of each warp writes
     cute.arch.barrier()
-    block_reduce_val = cute.BFloat16(float("-inf"))
+    block_reduce_val = init_val
     if lane_idx < warps_per_row:
         block_reduce_val = reduction_buffer[lane_idx]  # Only first `warps_per_row` lanes read
-    return cute.arch.warp_reduction_max(block_reduce_val)
+    return cute.arch.warp_reduction(block_reduce_val, op)
 
 
 @cute.jit
-def thread_reduction(
-    x: cute.Tensor,
-    op: cute.ReductionOp,
-) -> cute.Numeric:
-    x = x.load()  # cute.Tensor -> cute.TensorSSA
-    val = x.reduce(op, init_val=-cute.BFloat16.inf, reduction_profile=0)
+def thread_reduction(x: cute.TensorSSA, op: cute.ReductionOp, init_val: cute.Numeric = 0.0) -> cute.Numeric:
+    val = x.reduce(op, init_val=init_val, reduction_profile=0)
     return val
 
 
@@ -42,20 +41,32 @@ def _softmax_fwd(x: cute.Tensor, y: cute.Tensor, warps_per_row: cutlass.Constexp
     tidx, _, _ = cute.arch.thread_idx()
     bidx, _, _ = cute.arch.block_idx()
     bdim, _, _ = cute.arch.block_dim()
+    log2_e = 1.44269504
 
     smem = cutlass.utils.SmemAllocator()
     smem_layout = cute.make_layout((warps_per_row,))
-    sR = smem.allocate_tensor(cutlass.BFloat16, smem_layout)
+    sR = smem.allocate_tensor(cutlass.Float32, smem_layout)
 
     # bidx == [0..M), tidx == [0..N//vpt)
-    tile = x[(None, (bidx, tidx))]
-    tr = thread_reduction(tile, cute.ReductionOp.MAX)
-    vr = cute.arch.warp_reduction_max(tr)
-    block_max = block_reduce_single_row(vr, sR)
+    tile = x[(None, (bidx, tidx))].load()  # .load() cute.Tensor -> TensorSSA
 
-    # So now we have the block max, next we need to compute x_shift
-    x_shift = tile.load() - block_max
-    y[(None, (bidx, tidx))] = x_shift
+    # Max reduction, determine scaling factor for numerical
+    thread_max = thread_reduction(tile, cute.ReductionOp.MAX, cute.BFloat16(float("-inf")))  # 8 -> 1
+    warp_max = cute.arch.warp_reduction_max(thread_max)  # 32 -> 1
+    block_max = block_row_reduce(warp_max, lambda x, y: cute.arch.fmax(x, y), sR, cute.Float32(float("-inf")))  # N -> 1
+
+    x_shift = tile - block_max
+    x_exp = cute.exp2(log2_e * x_shift)
+
+    # Sum reduction, determine denominator
+    tr_exp_sum = thread_reduction(x_exp, cute.ReductionOp.ADD)  # 8 -> 1
+    warp_exp_sum = cute.arch.warp_reduction_sum(tr_exp_sum)  # 32 -> 1
+    denom = block_row_reduce(warp_exp_sum, lambda x, y: x + y, sR, cute.Float32(0.0))  # N -> 1
+
+    # Now divide it through
+    ytile = y[(None, (bidx, tidx))]
+    out = (x_exp / denom).to(cutlass.BFloat16)
+    ytile.store(out)
 
 
 """
@@ -89,8 +100,8 @@ if __name__ == "__main__":
 
     softmax(x_, y_)
 
-    ground = x - torch.max(x, dim=1)[0]
-    print(f"Ours: {y}")
-    print(f"Ground: {ground}")
+    ground = F.softmax(x, dim=-1)
+    # print(f"Ours: {y}")
+    # print(f"Ground: {ground}")
     torch.testing.assert_close(y, ground)
-    # print("Values match!")
+    print("Values match!")
