c174ab66dc9a7878d9b7e33e0c78fe68910acf37
FL33TW00D <chris@fleetwood.dev>
2026-03-05 19:08:25 +0000

    chore: clangd works

--- cuda_ramp/softmax.cu
+++ cuda_ramp/softmax.cu
@@ -4,18 +4,15 @@ void softmax_kernel(const float* input, float* output, int N) {
     int row = blockIdx.x;
     input += row * N;
     output += row * N;
-
     // Warp reduction is butterfly, each thread works together to pass it along to his bros
     // Sequential threads need to access sequential memory addresses
 
     // T __shfl_down_sync(unsigned mask, T var, unsigned int delta, int width=warpSize);
-    
     float max_val = -1e30f;
     for (int i = threadIdx.x; i < N; i += blockDim.x)
         max_val = fmaxf(max_val, input[i]);
     
     // So this thread now has the maximum of 4 values, and we can use shuffle down sync to get the full max
-    
     for (int offset = 16; offset > 0; offset >>= 1)
         max_val = fmaxf(max_val, __shfl_down_sync(0xffffffff, max_val, offset));
 
